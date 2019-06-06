f7cd5b0689ee9f454f1dc3b27590945709e4935f
Felipe Juchem <fajuchem@gmail.com>
2019-06-06 15:02:44 -0300

    sync

--- m0.c
+++ m0.c
@@ -5,6 +5,7 @@
 #include <complex.h>
 #include <unistd.h>
 #include <math.h>
+#include <curses.h>
 
 typedef struct Position {
     int y1;
@@ -20,12 +21,17 @@ int iterations = 2560;
 unsigned int mandelbrot(float x, float y);
 float **alloc_2d(int rows, int cols);
 
+/**
+ * - calcular y2, y1, x2, x1 dependendo do numero processo
+ * - ncurses fica com a tela de no maximo 80x24
+ * - processos recebem em ordem aleatoria mas são printados na tela em ordem
+ * - alocar dinamicamente buffer do recv quando um dos processos fica com um bloco maior
+ */
 int main(int argc, char *argv[])
 {
     int world_size, world_rank;
-    int height = 40;
-    int width = 120;
-    float **sub_arr;
+    int height = 24;
+    int width = 80;
 
     float delta[] = {
         (float)(view_x[1] - view_x[0]) / width,
@@ -42,10 +48,10 @@ int main(int argc, char *argv[])
     int columns = height / ceil(sqrt(world_size));
     int rows = width / ceil(sqrt(world_size));
 
-    columns = 5;
-    rows = 20;
+    columns = 4;
+    rows = 10;
 
-    printf("columns:%d, rows:%d\n", columns, rows);
+    //printf("columns:%d, rows:%d\n", columns, rows);
 
     //printf("height:%d, width:%d, column:%d, rows:%d\n", height, width, columns, rows);
     //return 0;
@@ -74,7 +80,7 @@ int main(int argc, char *argv[])
     int total_x = pos.x2 - pos.x1;
 
 
-    sub_arr = alloc_2d(total_y * total_x, 3);
+    float **sub_arr = alloc_2d(total_y * total_x, 3);
 
     int index = 0;
     for (int j = pos.y1; j < pos.y2; j++) {
@@ -99,20 +105,33 @@ int main(int argc, char *argv[])
 
     MPI_Isend(&(sub_arr[0][0]), total_y * total_x * 3, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &request);
 
+    free(sub_arr);
+    free(sub_arr[0]);
+
     if (world_rank == 0) {
         float **arr = alloc_2d(height, width);
-        for (int k = 0; k < world_size; k++) {
-            sub_arr = alloc_2d(total_y * total_x, 3);
+        float **sub_arr = alloc_2d(total_y * total_x, 3);
+        //initscr();
+        //curs_set(0);
 
-            printf("\n\ntotal_y:%d, total_x:%d\n\n", total_y, total_x);
+        for (int k = 0; k < world_size; k++) {
+
+            //printf("\n\ntotal_y:%d, total_x:%d\n\n", total_y, total_x);
 
             MPI_Recv(&(sub_arr[0][0]), total_y * total_x * 3, MPI_FLOAT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &stat);
+
+            //printf("world_rank:%d\n", stat.MPI_SOURCE);
 
             for (int i = 0; i < total_y * total_x; i++) {
                 int y = (int) sub_arr[i][0];
                 int x = (int) sub_arr[i][1];
                 //printf("x:%d, y:%d, v:%f\n", x, y, sub_arr[i][2]);
                 arr[y][x] = sub_arr[i][2];
+                //if ((int) sub_arr[i][2] == 0) {
+                //    mvaddch(y, x, '*');
+                //} else {
+                //    mvaddch(y, x, ' ');
+                //}
             }
 
             //for (int i = 0; i < total_y * total_x; i++) {
@@ -126,12 +145,23 @@ int main(int argc, char *argv[])
                         printf(" ");
                     }
                 }
-                printf("\n ");
+                printf("\n");
             }
-            //sleep(2);
+            //for (int j = 0; j < height; j++) {
+            //    for (int i = 0; i < width; i++) {
+            //        if ((int) arr[j][i] == 0) {
+            //            mvaddch(j, i, '*');
+            //        } else {
+            //            mvaddch(j, i, ' ');
+            //        }
+            //    }
+            //}
+            //refresh();
+            //sleep(1);
 
         }
-
+        //curs_set(1);
+        //endwin();
     }
 
     //printf("total_y:%d, total_x:%d \n", total_y, total_x);
